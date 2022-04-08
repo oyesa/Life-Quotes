87701beb5de7a8858fe150a48c446397393818b1
oyesa <mercyoyesa2018@gmail.com>
2022-04-08 19:21:25 +0300

    Add bootstrap and header component

---
+++ src/app/goal-header/goal-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-goal-header',
+  templateUrl: './goal-header.component.html',
+  styleUrls: ['./goal-header.component.css']
+})
+export class GoalHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
