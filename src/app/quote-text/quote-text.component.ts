434692a299a2e1bf0cd1923d7081667404d43dd3
oyesa <mercyoyesa2018@gmail.com>
2022-04-08 21:46:55 +0300

    add click functions on thumbs up and down

--- src/app/quote-text/quote-text.component.ts
+++ src/app/quote-text/quote-text.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-quote-text',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuoteTextComponent implements OnInit {
 
+  numberOfLikes! : number;
   constructor() { }
 
   ngOnInit(): void {
