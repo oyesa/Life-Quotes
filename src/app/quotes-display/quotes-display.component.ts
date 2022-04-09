c70a22558d118f55915fce36c01f4aeee181f810
oyesa <mercyoyesa2018@gmail.com>
2022-04-09 12:00:00 +0300

    update changes

--- src/app/quotes-display/quotes-display.component.ts
+++ src/app/quotes-display/quotes-display.component.ts
@@ -10,6 +10,33 @@ import { from} from 'rxjs'
 })
 export class QuotesDisplayComponent implements OnInit {
 
+  quotes: Quote[] = [
+    new Quote(0, 'African Intellectuals', 'Each language no matter how small carries its memory of the world', 0, 0)
+    new Quote(1, 'Jane Austen', 'A person may be proud without being vain, pride relates more to the opinion of ourselves, vanity to what we would have people think of us.', 0, 0)
+    new Quote(2, 'Charles Bukowski', '...But we joke and laugh otherwise we would start screaming.', 0, 0)
+  ]
+
+
+  toggleQuoteText(index){
+    this.quotes[index].showQuoteText=!this.quotes[index].showQuoteText
+  }
+  toggleQuoteForm(){
+    this.showQuoteForm=!this.showQuoteForm
+  }
+  deleteQuote(event, index){
+    if (event){
+      this.quotes.splice(index, 1)
+    }
+  }
+  
+  addQuote(newquote:Quote){
+    let quoted=this.quotes.length+1
+    newquote.id=quoteId 
+  
+    this.quotes.push(newquote)
+  }
+
+
   constructor() { }
 
   ngOnInit(): void {
@@ -17,31 +44,9 @@ export class QuotesDisplayComponent implements OnInit {
 
 }
 
-showQuoteForm: boolean=false
+// showQuoteForm: boolean=false
 
-quotes: Quote[] = [
-  new Quote(0, 'African Intellectuals', 'Each language no matter how small carries its memory of the world', 0, 0)
-  new Quote(1, 'Jane Austen', 'A person may be proud without being vain, pride relates more to the opinion of ourselves, vanity to what we would have people think of us.', 0, 0)
-  new Quote(2, 'Charles Bukowski', '...But we joke and laugh otherwise we would start screaming.', 0, 0)
-]
 
-// toggle quote
 
-toggleQuoteText(index){
-  this.quotes[index].showQuoteText=!this.quotes[index].showQuoteText
-}
-toggleQuoteForm(){
-  this.showQuoteForm=!this.showQuoteForm
-}
-deleteQuote(event, index){
-  if (event){
-    this.quotes.splice(index, 1)
-  }
-}
 
-addQuote(newquote:Quote){
-  let quoted=this.quotes.length+1
-  newquote.id=quoteId 
 
-  this.quotes.push(newquote)
-}
