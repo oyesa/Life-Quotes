e19aaf32c35b466e95cdc40a58d7243d291aad28
oyesa <mercyoyesa2018@gmail.com>
2022-04-09 10:54:51 +0300

    create component for quote form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { GoalHeaderComponent } from './goal-header/goal-header.component';
 import { QuoteTextComponent } from './quote-text/quote-text.component';
 import { QuotesDisplayComponent } from './quotes-display/quotes-display.component';
+import { QuotesFormComponent } from './quotes-form/quotes-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GoalHeaderComponent,
     QuoteTextComponent,
-    QuotesDisplayComponent
+    QuotesDisplayComponent,
+    QuotesFormComponent
   ],
   imports: [
     BrowserModule,
